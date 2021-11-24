fba28a10713092e6dba0127187bc5fd62cf6af95
egorovforjob <egorov_for_job@posteo.net>
2021-11-24 02:55:56 -0500

    test trigger trevis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TRY
+          Test not trigger
         </p>
         <a
           className="App-link"
